f556e3f3d81565449c67abea7ce93318947ca86e
Diolindo Luis <luis.diolindo@gmail.com>
2022-07-11 17:03:25 +0200

    Try fix zap rule

--- zap.sh
+++ zap.sh
@@ -21,8 +21,9 @@ exit_code=$?
 
 echo "Exit Code : $exit_code"
 
- if [[ ${exit_code} -ne 0 ]];  then
-    echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
-    exit 1;
+if [[ ${exit_code} -ne 0 ]];  
+then
+   echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
+   exit 1;
 else
-    echo "OWASP ZAP did not report any Risk"
+   echo "OWASP ZAP did not report any Risk"
