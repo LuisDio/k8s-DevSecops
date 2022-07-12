930d206a2e42ff7e29bc0de63b18b22dabc298ae
Diolindo Luis <luis.diolindo@gmail.com>
2022-07-12 16:46:21 +0200

    fix for integration test typo

--- integration-test.sh
+++ integration-test.sh
@@ -13,8 +13,8 @@ echo $applicationURL:$PORT/$applicationURI
 if [[ ! -z "$PORT" ]];
 then
 
-    response=$(curl -s $applicationURL:$PORT$applicationURI)
-    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
+    response=$(curl -s $applicationURL:$PORT/$applicationURI)
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT/$applicationURI)
 
     if [[ "$response" == 100 ]];
         then
